6cf72225c844a2bd94e39bcf8ee48c645efb0023
Nitesh <niteshreddy257@gamil.com>
2023-11-24 21:30:08 +0530

    hosted link front-end origin allowed

--- src/middleware/userAuth.middleware.js
+++ src/middleware/userAuth.middleware.js
@@ -1,15 +1,13 @@
 import jwt from "jsonwebtoken";
 const userVerification = async (req, res, next) => {
   try {
-   
-    if (!req.headers.authorization)
+    if (!req.headers.Authorization || !req.headers.authorization)
       res.status(404).json({ message: "Please send the authorization header" });
     else {
       let authorizationHeader =
         req.headers.authorization || req.headers.Authorization;
       authorizationHeader = authorizationHeader.split(" ")[1];
 
-     
       // check if the authorization header is exit or not
       if (!authorizationHeader)
         res.status(404).json({ message: "No token found" });
@@ -21,23 +19,19 @@ const userVerification = async (req, res, next) => {
         (err, decode) => {
           if (err) {
             res.status(401).json({ message: " unAuthorized user" });
-          } else 
-          {
-          const isOwner = decode.isOwner;
+          } else {
+            const isOwner = decode.isOwner;
 
-          
+            // the req object with decode value
+            req.user = decode;
 
-          // the req object with decode value
-          req.user = decode;
-
-          // check is the user is owner or not
-          if (isOwner)
-            res
-              .status(401)
-              .json({ message: "You are not authorized to do this" });
-        
+            // check is the user is owner or not
+            if (isOwner)
+              res
+                .status(401)
+                .json({ message: "You are not authorized to do this" });
           }
-         next();
+          next();
         }
       );
     }
